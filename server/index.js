df76559122bcc777c7c38d5f56d46a4f14a16a20
= <dogukanbasarann@hotmail.com>
2024-01-26 22:53:26 +0300

    create a test api route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -17,6 +18,9 @@ mongoose.connect(process.env.MONGO)
 )
 
 const app = express();
+
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
 });
+
+app.use('/server/user', userRoutes);
