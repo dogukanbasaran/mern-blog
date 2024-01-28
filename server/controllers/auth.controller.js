6b9e59d26b120daed15473c9f73d8bed5bc63ddb
= <dogukanbasarann@hotmail.com>
2024-01-28 06:05:46 +0300

    create sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-
+import jwt from 'jsonwebtoken';
 import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
@@ -25,4 +25,33 @@ export const signup = async (req, res, next) => {
         next(error);
     }
     
+}
+
+export const signin = async (req, res, next) => {
+
+    const {username, password} = req.body;
+
+    if (!username || !password || username === '' || password === ''){
+           return  next(errorHandler(400, 'All fields are required.'));
+    }
+    
+    try {
+        const validUser = await User.findOne({username});
+        if (!validUser){
+           return next(errorHandler(404, 'User not found!'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+           return next(errorHandler(400, 'Invalid password!'))
+        }
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+
+        const {password: pass, ...rest} = validUser._doc;
+
+        res.status(200).cookie('access_token', token, {httpOnly: true}).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
 }
