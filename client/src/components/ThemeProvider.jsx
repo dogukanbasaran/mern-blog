b6ec436aacbaab9884fbab66b4ab8325aa584f2d
= <dogukanbasarann@hotmail.com>
2024-01-28 14:05:57 +0300

    theme updating

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -4,7 +4,7 @@ export default function ThemeProvider({children}) {
     const {theme} = useSelector(state => state.theme)
   return (
     <div className={theme}>
-      <div className='bg-slate-50 text-gray-800 dark:text-gray-200 dark:bg-slate-950 min-h-screen'>
+      <div className='bg-gray-100 text-neutral-800 dark:text-neutral-100 dark:bg-gray-800 min-h-screen'>
         {children}
       </div>
     </div>
